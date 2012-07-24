60ae2c53d7991d31b4b628cc4801ca34e8f66b4b
seedorff <seedorff@caelum.dk>
2012-07-24 20:36:01 +0200

    change slider options

--- wp-content/themes/Foundation/javascripts/orbit-1.3.0.js
+++ wp-content/themes/Foundation/javascripts/orbit-1.3.0.js
@@ -13,21 +13,21 @@
     
     defaults: {  
       animation: 'horizontal-push', 		// fade, horizontal-slide, vertical-slide, horizontal-push, vertical-push
-      animationSpeed: 600, 				// how fast animtions are
+      animationSpeed: 800, 				// how fast animtions are
       timer: true, 						// true or false to have the timer
       advanceSpeed: 4000, 				// if timer is enabled, time between transitions 
-      pauseOnHover: false, 				// if you hover pauses the slider
-      startClockOnMouseOut: false, 		// if clock should start on MouseOut
+      pauseOnHover: true, 				// if you hover pauses the slider
+      startClockOnMouseOut: true, 		// if clock should start on MouseOut
       startClockOnMouseOutAfter: 1000, 	// how long after MouseOut should the timer start again
-      directionalNav: true, 				// manual advancing directional navs
+      directionalNav: false, 				// manual advancing directional navs
       captions: true, 					// do you want captions?
       captionAnimation: 'fade', 			// fade, slideOpen, none
       captionAnimationSpeed: 600, 		// if so how quickly should they animate in
-      bullets: false,						// true or false to activate the bullet navigation
+      bullets: true,						// true or false to activate the bullet navigation
       bulletThumbs: false,				// thumbnails for the bullets
       bulletThumbLocation: '',			// location from this file where thumbs will be
       afterSlideChange: $.noop,		// empty function 
-      fluid: true,
+      fluid: 16x9,
       centerBullets: true    // center bullet nav with js, turn this off if you want to position the bullet nav manually
  	  },
  	  
